83321538e9e005e9b675c45241a8b2e1dc0aaab8
mangesh <kokaremangesh@gmail.com>
2024-02-26 11:42:27 +0530

    initial commit

---
+++ src/main/java/com/petstoreapp/petapp/PetappApplication.java
@@ -0,0 +1,13 @@
+package com.petstoreapp.petapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetappApplication.class, args);
+	}
+
+}
